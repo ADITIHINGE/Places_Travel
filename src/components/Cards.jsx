8dd8d7ec62d1d1f805c324e0ad88fbde269c68c3
Aditi Hinge <aditihinge287@gmail.com>
2022-10-21 22:32:08 +0530

    update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,10 +7,10 @@ const Cards = (props) => {
     <div className= {styles.card} >
       <img src={place.img} alt='place' className={styles.img} />
       <p><b>{place.Title}</b></p>
-      {/* <p><i>{place.Description}</i></p>
+      <p><i>{place.Description}</i></p>
       <p> <h4> Price: ${place.Price} </h4></p>
-      <p><h4> Rate: {place.Rating}/5 </h4></p> */}
-     {/* <button className={styles.btn}>Book Now</button> */}
+      <p><h4> Rate: {place.Rating}/5 </h4></p> 
+      <button className={styles.btn}>Book Now</button>
     </div>
   )
 }
